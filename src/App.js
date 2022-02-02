67c93deefd67ac3f42d8ec1ec4cc5d15a7bd8a76
argun8394 <argundoganali@gmail.com>
2022-02-03 02:07:26 +0300

    react_hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Header from "./components/Header";
+// import List from "./components/List";
+// import aws from "assets/aws.png";
+// import fs from "assets/fs.png";
 
 function App() {
+  const [counter,setCounter] =useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <p>counter : {counter}</p>
+      <button onClick={()=>setCounter(counter+1)}>Increase</button>
+     
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import cw from "../assets/cw_logo.png";
+
+const Header =({img})=>{
+    console.log("Rendering : Header Comp!");
+    return(
+        <div className="header">
+            <img src={img ? img : cw}
+            alt={"cw_logo"}
+            style={{margin:"1rem", maxHeight: "200px"}}
+            />
+        </div>
+    )
+};
+
+export default Header;
